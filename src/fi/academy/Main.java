bf764801d1f584c10bdd16d3e330ee8701a3d9e3
TommiS <tommi.supi@gmail.com>
2020-01-14 13:31:34 +0200

    lisätty tulostus komento

--- src/fi/academy/Main.java
+++ src/fi/academy/Main.java
@@ -3,5 +3,6 @@ package fi.academy;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Pull request is the best!");
+        System.out.println("Yes it is!");
     }
 }
